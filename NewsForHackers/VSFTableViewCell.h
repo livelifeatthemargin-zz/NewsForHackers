eb6da8c473ecaed934cc0b7c47c4a5b3d7c75595
livelifeatthemargin <vincent@runa.com>
2014-05-16 23:46:17 -0700

    Now Grabs largest image from each HN link

---
+++ NewsForHackers/VSFTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  VSFTableViewCell.h
+//  NewsForHackers
+//
+//  Created by vincentsfong on 5/16/14.
+//  Copyright (c) 2014 Vincent Fong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSFTableViewCell : UITableViewCell
+
+@end
---
+++ NewsForHackers/VSFTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  VSFTableViewCell.m
+//  NewsForHackers
+//
+//  Created by vincentsfong on 5/16/14.
+//  Copyright (c) 2014 Vincent Fong. All rights reserved.
+//
+
+#import "VSFTableViewCell.h"
+
+@implementation VSFTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
